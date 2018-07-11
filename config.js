2d1ecbf35907ee9dde1eac1d2edb541ae045106b
pchetia3 <pchetia3@gatech.edu>
2018-07-11 19:13:22 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,4 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/6439c083-955d-474c-a758-cceac462f5bc/token";
+const instanceLocator = "v1:us1:6439c083-955d-474c-a758-cceac462f5bc";
+
+export { tokenUrl, instanceLocator }
